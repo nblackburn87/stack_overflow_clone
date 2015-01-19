9c6de34d0f7c758228225c257d396da631877cde
Natalie <blackburn87@gmail.com>
2015-01-18 19:24:24 -0800

    add missing end to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-private
-  def user_params
-    params.require(:user).permit(:email, :password)
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+  end
 end
